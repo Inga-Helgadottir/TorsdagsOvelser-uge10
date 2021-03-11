a8a9fa716b92ed9fb6b78eefe6aa1a16e90c6f0a
Inga-Helgadottitr <78418864+Inga-Helgadottitr@users.noreply.github.com>
2021-03-11 15:51:38 +0100

    Update Main.java

--- OOP/Task1/src/com/company/Main.java
+++ OOP/Task1/src/com/company/Main.java
@@ -5,7 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Driver driver1 = new Driver("Inga", 25);
         Car car1 = new Car("make", "model", 2000, "bodyStyle", driver1);
+        Car car2 = new Car("make2", "model2", 2000, "bodyStyle2", driver1);
         System.out.println(car1.toString());
+        System.out.println(driver1.toString());
+
+        System.out.println(car2.toString());
         System.out.println(driver1.toString());
     }
 }
